1560cec3245c29c1707089ec38f763b7ad6ec8f3
Austin Ohlfs <ohlfsam96@gmail.com>
2023-09-11 21:42:04 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+// Write your code inthis file!
+let currentUser = `You need to modify the value of the ${currentUser} variable`;
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let upperCaseCurrentUser = currentUser.toUpperCase();
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+const firstInital = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+let shortGreeting = `Welcome, ${firstInital}!`;
